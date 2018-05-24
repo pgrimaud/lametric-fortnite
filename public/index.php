03ddc9a3441bef9b0a79e090996120e565e6a608
Pierre Grimaud <pierre.grimaud@bigyouth.fr>
2018-05-24 14:06:13 +0200

    App wip

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Fortnite\Auth;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $credentialsFile = __DIR__ . '/../config/credentials.php';
@@ -7,6 +9,25 @@ $credentialsFile = __DIR__ . '/../config/credentials.php';
 if (is_file($credentialsFile)) {
     $credentials = require __DIR__ . '/../config/credentials.php';
 
+    echo '{
+    "frames": [
+        {
+            "text": "ortnite",
+            "icon": "i20640"
+        }
+    ]
+}';
+
+//    $auth = Auth::login($credentials['email'], $credentials['password']);
+//
+//    $user = 'Nilzèn';
+//    //$user = 'ZiiKoO';
+//
+//    $sandy = $auth->profile->stats->lookup($user);
+//
+//    print_r($sandy);
+//
+//    echo $user . ' has won ' . $sandy->pc->solo->wins . ' solo games, ' . $sandy->pc->duo->wins . ' duo games and ' . $sandy->pc->squad->wins . ' squad games!';
 
 } else {
     throw new \Fortnite\Exception\ConfigException('Missing config file');
